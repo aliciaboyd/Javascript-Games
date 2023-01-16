556a6d7053427052d95486338a6b94b5d5c9d919
Alicia Boyd <26505495+aliciaboyd@users.noreply.github.com>
2023-01-15 20:43:32 -0500

    Adds base HTML and CSS styles

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+  1. User clicks button to start quiz
+  2. Randomly pulled quiz question appears
+  3. Visual indication of correct/wrong answer
+    3.5. Next button when question is answered
+  4. Counter increases with correct answer
+  5. Next question is displayed until out of questions
+  6. End of quiz displays start over button and result
+*/
+
+
+//variable to store # of correct questions
+//variable that compares answered question to correct answer
+//next button that pulls next question information
+//end screen on no more available questions
